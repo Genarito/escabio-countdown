55ab5ec99b9a7655f028b2e6b092f79678271fab
FaacuuDiaz <facu_tls95@hotmail.com>
2019-02-26 20:19:39 -0300

    subiendo ronda relampago

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+import logo from '../lightning.gif'
 import '../css/index.css';
 import '../bootstrap-3.3.7-dist/css/bootstrap.min.css';
 
@@ -9,9 +10,8 @@ class Escabio extends React.Component {
         super(props);
 
         this.drinks = ['Fernet', 'Vodka'];
-
         this.state = {
-            countdown: 600,
+            countdown: 10,
             loserName: '',
             drink: '',
             names: [
@@ -26,9 +26,15 @@ class Escabio extends React.Component {
                 'El ema',
                 'Ruso',
                 'Nuno'
-                ]
+            ]
         };
 
+        // variables and array for the lightning round
+        this.lightningNames = this.state.names.slice();
+        this.shotRound= false;
+        this.lightning = 0; 
+        this.common = 3; //for testing of lightningRound change this value to 3 and de countdown decrease to 10
+        
         // Bind 'this' variable to methods which are called from view
         this.handleInput = this.handleInput.bind(this);
         this.add = this.add.bind(this);
@@ -91,10 +97,22 @@ class Escabio extends React.Component {
         } else {
             countdownDescripcion = seconds + ' segundos';
         }
-
-        if (!minutes && seconds == '00') {
-            this.getLoser();
+        if(this.common >=2){
+            this.shotRound = true;
         }
+        if(!minutes && seconds == '00') {
+            if (this.common < 3 ) {
+                this.getLoser();
+                this.common= this.common + 1;
+                this.shotRound=false;
+            }
+            else{
+                if(this.lightning < 4){
+                    this.lightningRound();
+                    this.lightning = this.lightning + 1;
+                }
+            }
+        }    
 
         return countdownDescripcion;
     }
@@ -120,24 +138,69 @@ class Escabio extends React.Component {
         }, 20000);
     }
 
+    lightningRound(){
+        let randomNameIndex = Math.floor(Math.random() * (this.lightningNames.length));
+        let randomDrinkIndex = Math.floor(Math.random() * (this.drinks.length));
+        let getLoser = this.lightningNames[randomNameIndex];
+        let countd = 3
+        let aux = this.lightningNames.splice(randomNameIndex,1);
+        if(this.lightning == 3 ){
+            countd = 600;
+            this.common = 0;
+            this.lightning = 0;
+            this.shotRound=false;
+            this.lightningNames = this.state.names.slice()
+        }
+        this.setState({
+            countdown: countd,
+            loserName: getLoser,
+            drink: this.drinks[randomDrinkIndex]
+        });
+        getLoser='';
+        setTimeout(() => {
+            this.setState({
+                loserName: '',
+                drink: ''
+            });
+        }, 20000);
+
+    }
+
     render() {
         const namesList = this.state.names.map((name, idx) => idx < this.state.names.length - 1 ? name + ', ' : name);
+        let image;
+        let id_h1;
+        let id_timer;
+        let pera;
+        if(this.shotRound){
+            image = <img src={logo} alt="...loading" className="image"/>
+            id_h1='lightning';
+            id_timer='lightning';
+            pera= <span>en la pera con</span>;
+        }
+        else{
+            image='';
+            id_h1='none';
+            id_timer='timer';
+            pera= 'en la pera con'
+        }
 
         return (
             <div>
+                {image}
                 <div className="row">
                     <div className="col-md-12 text-center">
-                        <h1>Fondo en</h1>
-                        <h1 id="timer">{this.generateCountdown()}</h1>
+                        <h1 className={id_h1}>Fondo en</h1>
+                        <h1 id={id_timer}>{this.generateCountdown()}</h1>
                         {this.state.loserName &&
-                            <h1 id="loser"><strong className="danger">{this.state.loserName}</strong> en la pera con <strong className="danger">{this.state.drink}</strong></h1>
+                            <h1 id="loser"><strong className="danger">{this.state.loserName}</strong> {pera} <strong className="danger">{this.state.drink}</strong></h1>
                         }
                     </div>
                 </div>
                 <div id="input-name" className="row">
                     <div className="col-md-12 text-center">
                         <input type="text" value={this.state.newName} onChange={this.handleInput}/>
-                        <button className="btn btn success" onClick={this.add}>Agregar</button>
+                        <button className="btn btn-success" onClick={this.add}>Agregar</button>
                     </div>
                 </div>
                 <div className="row">
@@ -147,6 +210,7 @@ class Escabio extends React.Component {
                     </div>
                 </div>
             </div>
+
         );
     }
 }
